d3cf9e93f0ae7c652cadd28f3ec2491fc094c90f
Miguel Verdelho <miguel@verdelho.net>
2025-03-30 02:17:44 +0100

    photos with galery

--- traveler-app/src/app/components/photo-modal/photo-modal.component.ts
+++ traveler-app/src/app/components/photo-modal/photo-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, AfterViewInit, OnDestroy, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HostListener } from '@angular/core';
+import PhotoSwipeLightbox from 'photoswipe/lightbox';
+import 'photoswipe/style.css';
 
 @Component({
   selector: 'app-photo-modal',
@@ -8,7 +11,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './photo-modal.component.html',
   styleUrls: ['./photo-modal.component.css']
 })
-export class PhotoModalComponent {
+export class PhotoModalComponent implements AfterViewInit, OnDestroy {
   @Input() photos: string[] = [];
   @Input() description: string = '';
   @Input() link: string = '';
@@ -17,23 +20,64 @@ export class PhotoModalComponent {
   @Input() isVisible: boolean = false;
   @Output() onClose = new EventEmitter<void>();
 
-
   currentIndex: number = 0;
+  lightbox?: PhotoSwipeLightbox;
 
+  @ViewChild('galleryContainer', { static: false }) galleryContainer!: ElementRef;
+  @HostListener('document:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent): void {
+    if (!this.isVisible) return;
+  
+    switch (event.key) {
+      case 'ArrowRight':
+        this.next();
+        break;
+      case 'ArrowLeft':
+        this.prev();
+        break;
+      case 'Escape':
+        this.onClose.emit(); // optional: allow ESC to close modal
+        break;
+    }
+  }
 
   get currentPhoto(): string {
     return this.photos[this.currentIndex];
   }
 
-  next() {
+  ngAfterViewInit(): void {
+    this.lightbox = new PhotoSwipeLightbox({
+      gallery: '.image-area',
+      children: 'a',
+      pswpModule: () => import('photoswipe')
+    });
+    this.lightbox.init();
+  }
+
+  ngOnDestroy(): void {
+    this.lightbox?.destroy();
+  }
+
+  next(): void {
     this.currentIndex = (this.currentIndex + 1) % this.photos.length;
   }
 
-  prev() {
+  prev(): void {
     this.currentIndex = (this.currentIndex - 1 + this.photos.length) % this.photos.length;
   }
 
-  goTo(index: number) {
+  goTo(index: number): void {
     this.currentIndex = index;
   }
+
+  openGallery(event: Event): void {
+    event.preventDefault();
+
+    if (this.lightbox?.pswp) {
+      this.lightbox.pswp.close();
+    }
+
+    this.lightbox?.loadAndOpen(this.currentIndex);
+  }
+  
 }
